1ed55538610d89e63131ca6a9db3c145561bdb62
Marin <marinmema@live.ca>
2025-11-28 19:26:55 -0500

    add comments

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const PROTOCOL = "ws";
 const HOST = "localhost";
 const PORT = 3001;
 
-const wss = new WebSocketServer({ port: 3001 });
+const server = new WebSocketServer({ port: 3001 });
 
 interface BroadcastWebSocket extends WebSocket {
   connectionId?: string;
@@ -13,17 +13,27 @@ interface BroadcastWebSocket extends WebSocket {
   echo?: boolean;
 }
 
-wss.on("listening", () =>
+server.on("listening", () =>
   console.log(`Listening at ${PROTOCOL}://${HOST}:${PORT}...`),
 );
 
-wss.on("error", console.error);
-wss.on("connection", (ws: BroadcastWebSocket, request) => {
+server.on("error", console.error);
+
+server.on("connection", (ws: BroadcastWebSocket, request) => {
+  // Assign a unique connection ID
   ws.connectionId = crypto.randomUUID();
+
+  // Set the channel they want to join.
   const url = new URL(request.url ?? "", `${PROTOCOL}://${HOST}`);
   ws.channel = url?.searchParams.get("channel") ?? "";
+
+  // The client must specifically connect with a ?echo=false query param to opt
+  // out of echoes. Otherwise, they will get all of their own messages echoed
+  // back to them.
   ws.echo = (url?.searchParams.get("echo") ?? "") !== "false";
+
   console.log(`new connection on channel "${ws.channel}"`);
+
   ws.on("message", (data, isBinary) => broadcast(ws, data, isBinary));
   ws.on("error", console.error);
 });
@@ -34,7 +44,7 @@ function broadcast(
   isBinary: boolean,
 ) {
   console.log(`connectionId ${sender.connectionId} channel ${sender.channel}: %s`, data);
-  wss.clients.forEach((ws: BroadcastWebSocket) => {
+  server.clients.forEach((ws: BroadcastWebSocket) => {
     if (
       ws.readyState === WebSocket.OPEN &&
       ws.channel === sender.channel &&
